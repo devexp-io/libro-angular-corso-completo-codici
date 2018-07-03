c723f24f7869ec851bd2013ebdba7be914c0a6f8
Salvatore Romeo <me@salvatoreromeo.com>
2018-07-03 23:32:32 +0200

    Aggiustamento path per la DEMO online

--- devexp11/src/app/app.module.ts
+++ devexp11/src/app/app.module.ts
@@ -21,7 +21,7 @@ import {TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, "/assets/i18n/", ".json");
+  return new TranslateHttpLoader(http, "assets/i18n/", ".json");
 }
 
 const routes = [
